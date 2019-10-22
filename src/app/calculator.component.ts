e15a43925e8115dd12a08672c35054b85d726a26
Nicolas Mancilla Vergara <nico@nicomv.com>
2019-10-22 19:00:58 -0300

    Finished basic functionality

--- src/app/calculator.component.ts
+++ src/app/calculator.component.ts
@@ -10,10 +10,10 @@ export default class Calculator implements AfterViewInit {
   operation: string = '0';
   nums = Array.from(Array(10), (x, i) => i);
   buttons: Array<Object> = [];
-  expression: Array<string> = [];
-  lastOperation: string = '';
   operators: Array<string> = ['-', '+', '*', '/'];
   negate = false;
+  expr: string = '0';
+  result: string = '';
 
   constructor() {
     this.buttons.push({
@@ -61,47 +61,58 @@ export default class Calculator implements AfterViewInit {
 
   ngAfterViewInit() {}
 
-  updateOperation(operand: string) {
-    console.debug('Operand:', operand);
-    this.operation += operand;
+  clear(clearResult = true) {
+    this.operation = '0';
+    this.expr = '0';
+    if (clearResult) {
+      this.result = '';
+    }
   }
 
   onClear() {
-    this.operation = '0';
-    this.expression = [];
-    this.lastOperation = '';
+    this.clear();
   }
 
   onButtonClick(event: any) {
     const value = event.target.value;
+    console.debug('Button value:', value);
+
+    if (this.result) {
+      this.expr = '';
+      this.result = '';
+    }
+
     if (this.operation === '0') {
       this.operation = value;
-      console.debug('Expression:', this.expression);
-      return;
+      this.expr = value;
+    } else {
+      this.operation += value;
+      this.expr += value;
     }
-    if (this.negate === false && this.isOperator(this.operation)) {
-      console.debug('Current operation:', this.operation);
-      this.operation = '';
-    }
-    this.operation += value;
-    console.debug('Expression:', this.expression);
   }
 
   onOperatorClick(event: any) {
-    console.debug('Event:', event);
-    const operator = event.target.value;
-    console.debug('Operator:', operator);
-    const lastIndex = this.expression.length - 1 || 0;
-    this.expression.push(this.operation);
+    const value = event.target.value;
+    console.debug('Operator click:', value);
 
-    if (this.isOperator(this.expression[lastIndex])) {
-      this.expression[lastIndex] = operator;
-    } else {
-      this.expression.push(operator);
+    if (this.result) {
+      this.expr = this.result;
+      this.result = '';
     }
 
-    this.operation = operator;
-    console.debug('Expression:', this.expression);
+    const canAdd = this.canAddOperator(value);
+
+    if (canAdd === 1) {
+      console.debug('Appending operator');
+      this.expr += value;
+    } else if (canAdd === 0) {
+      console.debug('Replacing operator');
+      this.expr = this.expr.slice(0, -1) + value;
+    } else {
+      this.expr = this.expr.slice(0, -2) + value;
+    }
+
+    this.operation = value;
   }
 
   onDecimalClick() {
@@ -109,15 +120,16 @@ export default class Calculator implements AfterViewInit {
     if (this.operation.includes(dot)) {
       return;
     }
-    this.updateOperation(dot);
+    this.operation += dot;
+    this.expr += dot;
   }
 
   onEqualsClick() {
-    if (!this.isOperator(this.operation)) {
-      this.expression.push(this.operation);
-      this.operation = '';
-    }
-    this.updateResult();
+    this.operation = '';
+    this.expr = this.expr.replace(/(\s+[\-+\/\*]\s+$|(=\d+))/, '');
+    this.result = evaluate(this.expr);
+    this.expr = `${this.expr}=${this.result}`;
+    this.operation = this.result;
   }
 
   onNegate() {
@@ -130,13 +142,40 @@ export default class Calculator implements AfterViewInit {
     }
   }
 
-  updateResult() {
-    console.debug('Entering updateResult');
-    console.debug('Expression:', this.expression);
-    this.operation = evaluate(this.expression.join('')).toString();
-    this.expression = [];
-    console.debug('Updated expression:', this.expression);
-    console.debug('Leaving updateResult');
+  onDelClick() {
+    if (this.expr.length > 0) {
+      this.expr = this.expr.slice(0, -1);
+      this.operation = '';
+      if (this.expr.length === 0) {
+        this.clear(false);
+      }
+    }
+  }
+
+  /**
+   * Checks if the operator can be added or not.
+   * Restrictions:
+   *
+   * - No more than two consecutive operators
+   * - The only operator that can be repeated twice is minus
+   * - Any operator can be followed by the minus.
+   * @param val The val to check.
+   */
+  canAddOperator(val: string): number {
+    const lastExpression = this.expr[this.expr.length - 1];
+    const lastIsOperator = this.isOperator(lastExpression);
+
+    if (!lastIsOperator) {
+      return 1;
+    }
+
+    if (val === '-') {
+      if (lastIsOperator && !this.isOperator(this.expr[this.expr.length - 2])) {
+        return 1;
+      }
+    }
+
+    return this.isOperator(this.expr[this.expr.length - 2]) ? -1 : 0;
   }
 
   isOperator(operator: string) {
